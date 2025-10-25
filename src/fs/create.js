fdb97ab6da5bd4acf04689c577133f9b3496de0a
Mikhail Kirpichou <mishania261282@.ru>
2025-10-25 18:55:33 +0300

    feat:add a solution for the file create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,28 @@
+import path from "path";
+import { writeFile } from "fs/promises";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+function getFileName() {
+  return fileURLToPath(import.meta.url);
+}
+
+function getDirName() {
+  return dirname(getFileName());
+}
+
+const __filename = getFileName();
+const __dirname = getDirName();
+const filePath = path.join(__dirname, "files", "fresh.txt");
+const content = "I am fresh and young";
+
+
 const create = async () => {
-  // Write your code here
+  try {
+    await writeFile(filePath, content, { flag: "wx" });
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await create();
