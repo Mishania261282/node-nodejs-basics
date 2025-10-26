23d237896e0290426d9017e963b59c4fd5ab3420
Mikhail Kirpichou <mishania261282@.ru>
2025-10-26 21:31:26 +0300

    feat:add the solution for the file stream/write.exe

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,27 @@
-const write = async () => {
-  // Write your code here
+import { createWriteStream } from "node:fs";
+import process from "node:process";
+import path from "node:path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const fileName = "fileToWrite.txt";
+const filePath = path.join(__dirname, "files", fileName);
+
+const write = () => {
+  const writeStream = createWriteStream(filePath, {
+    encoding: "utf8",
+    autoClose: true,
+    emitClose: true,
+  });
+
+  writeStream.on("close", () => {
+    console.log("");
+  });
+
+  process.stdin.pipe(writeStream);
 };
 
-await write();
+console.log(
+  `Please type your text and press Enter and then Ctrl+C when done.`
+);
+write();
