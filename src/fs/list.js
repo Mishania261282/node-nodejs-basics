49873b324f029a500d17af2159c15b19583f2dd7
Mikhail Kirpichou <mishania261282@.ru>
2025-10-26 16:59:36 +0300

    feat:add the solution for the file list.exe

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,23 @@
+import { promises, constants } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const folderName = "files";
+const folderPath = path.join(__dirname, folderName);
+
 const list = async () => {
-  // Write your code here
+  try {
+    await promises.access(folderPath, constants.F_OK);
+    const filesAndDirs = await promises.readdir(folderPath);
+    console.log("Содержимое папки 'files':");
+    console.log(filesAndDirs);
+  } catch (error) {
+    if (error.code === "ENOENT" && error.path === folderPath) {
+      console.log("Папки 'files' не существует.");
+      console.error("FS operation failed");
+    }
+  }
 };
 
-await list();
+list();
