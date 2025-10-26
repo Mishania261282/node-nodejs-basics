495cb10082e9a8b3c50ac71d6c0aefbf19c2c605
Mikhail Kirpichou <mishania261282@.ru>
2025-10-26 22:30:25 +0300

    feat:add the solution for the file decompress.exe

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,33 @@
-const decompress = async () => {
-  // Write your code here
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGunzip } from 'node:zlib';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+import { pipeline } from 'node:stream';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const sourceArchiveFileName = 'archive.gz';
+const decompressedFileName = 'fileToCompress.txt';
+const sourceArchiveFilePath = path.join(__dirname, sourceArchiveFileName);
+const decompressedFilePath = path.join(__dirname, decompressedFileName);
+
+const decompress = () => {  
+  const readStream = createReadStream(sourceArchiveFilePath);
+  const gunzipStream = createGunzip();  
+  const writeStream = createWriteStream(decompressedFilePath);
+
+  pipeline(
+    readStream,
+    gunzipStream,
+    writeStream,
+    (err) => {
+      if (err) {        
+        process.exit(1);
+      } else {
+        console.log(`Successfully decompressed ${sourceArchiveFileName} to ${decompressedFileName}.`);
+        process.exit(0);
+      }
+    }
+  );
 };
 
-await decompress();
+decompress();
