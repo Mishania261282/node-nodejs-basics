e6c48ff65a9db111cd5af8696b5be3edb993a5b7
Mikhail Kirpichou <mishania261282@.ru>
2025-10-26 22:24:52 +0300

    feat:add the solution for the file compress.exe

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,31 @@
-const compress = async () => {
-  // Write your code here
+import { createReadStream, createWriteStream } from "node:fs";
+import { createGzip } from "node:zlib";
+import path from "node:path";
+import { fileURLToPath } from "url";
+import { pipeline } from "node:stream";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const sourceFileName = "fileToCompress.txt";
+const archiveFileName = "archive.gz";
+const sourceFilePath = path.join(__dirname, "files", sourceFileName);
+const archiveFilePath = path.join(__dirname, archiveFileName);
+
+const compress = () => {
+  const readStream = createReadStream(sourceFilePath);
+  const gzipStream = createGzip();
+  const writeStream = createWriteStream(archiveFilePath);
+
+  pipeline(readStream, gzipStream, writeStream, (err) => {
+    if (err) {
+      console.error(`Compression failed: ${err.message}`);     
+      process.exit(1);
+    } else {
+      console.log(
+        `Successfully compressed ${sourceFileName} to ${archiveFileName}.`
+      );
+      process.exit(0);
+    }
+  });
 };
 
-await compress();
+compress();
