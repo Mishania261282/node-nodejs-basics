ff3b15807b5bdd18449cf8bf7f5489fffaef8218
Mikhail Kirpichou <mishania261282@.ru>
2025-10-26 17:41:34 +0300

    feat:add the solution for the file env.exe

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
 const parseEnv = () => {
-  // Write your code here
+  const prefix = "RSS_";
+  const argEnv = [];
+
+  for (const key in process.env) {
+    if (key.startsWith(prefix)) {
+      argEnv.push(`${key}=${process.env[key]}`);
+    }
+  }
+
+  const result = argEnv.join("; ");
+  console.log(result);
 };
 
 parseEnv();
