33055f14d9862bbe568df2f48b2b07105e4e6583
Mikhail Kirpichou <mishania261282@.ru>
2025-10-26 22:07:39 +0300

    feat:add the solution for the file stream/transform.exe

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,36 @@
-const transform = async () => {
-  // Write your code here
+import { Transform } from 'node:stream';
+import process from 'node:process';
+import { pipeline } from 'node:stream';
+
+
+class ReverseTextTransform extends Transform {  
+  _transform(chunk, encoding, callback) {
+    const inputString = chunk.toString();
+    const reversedChunk = inputString.split('').reverse().join('');    
+    
+    this.push(reversedChunk);     
+    callback(); 
+  }
+}
+
+const transform = () => {
+  const reverseStream = new ReverseTextTransform();
+
+  console.log('Please type text and press Enter.');
+
+  pipeline(
+    process.stdin,      
+    reverseStream,      
+    process.stdout,     
+    (err) => {
+      if (err) {
+        console.error(`Pipeline failed: ${err.message}`);
+        process.exit(1);
+      } else {        
+        process.exit(0);
+      }
+    }
+  );
 };
 
-await transform();
+transform();
