fde7e7f7709732adff6ecfabf07219d5ee26e227
Mikhail Kirpichou <mishania261282@.ru>
2025-10-26 17:11:02 +0300

    feat:add the solution for the file read.exe

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,26 @@
+import { promises, constants } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const fileName = "fileToRead.txt";
+const filePath = path.join(__dirname, "files", fileName);
+
 const read = async () => {
-  // Write your code here
+  try {
+    await promises.access(filePath, constants.F_OK);
+
+    const content = await promises.readFile(filePath, { encoding: "utf8" });
+
+    console.log(`Содержимое файла ${fileName}`);
+    console.log(content);
+  } catch (error) {
+    if (error.code === "ENOENT" && error.path === filePath) {
+      console.log(`Такого файла '${fileName}' не существует.`);
+      console.error("FS operation failed");
+    }
+  }
 };
 
-await read();
+read();
