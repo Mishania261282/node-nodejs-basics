f0d25d9d602cad9d80cc5a7190dc44fb10225247
Mikhail Kirpichou <mishania261282@.ru>
2025-10-26 16:47:46 +0300

    feat:add the solution for the file delete.exe

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,24 @@
+import { promises, constants } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const fileName = "fileToRemove.txt";
+const filePath = path.join(__dirname, "files", fileName);
+
 const remove = async () => {
-  // Write your code here
+  try {
+    await promises.access(filePath, constants.F_OK);
+    await promises.unlink(filePath);
+    console.log(`Файл '${fileName}' успешно удален.`);
+    process.exit(0);
+  } catch (error) {
+    if (error.code === "ENOENT" && error.path === filePath) {
+      console.log(`Такого файла '${fileName}' не существует.`);
+      console.error("FS operation failed");
+    }
+    process.exit(1);
+  }
 };
 
-await remove();
+remove();
