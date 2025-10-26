7972c74fac41f3eff72960b58230c5fae5c5df36
Mikhail Kirpichou <mishania261282@.ru>
2025-10-26 19:00:16 +0300

    feat:add the solution for the file hash.exe

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { createReadStream } from "node:fs";
+import { createHash } from "node:crypto";
+import path from "node:path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const fileName = "fileToCalculateHashFor.txt";
+const filePath = path.join(__dirname, "files", fileName);
+
 const calculateHash = async () => {
-  // Write your code here
+  const hash = createHash("sha256");
+  const readStream = createReadStream(filePath);
+
+  readStream.on("data", (chunk) => {
+    hash.update(chunk);
+  });
+
+  readStream.on("end", () => {
+    const hexHash = hash.digest("hex");
+    console.log(`SHA256 Hash for ${fileName}:`);
+    console.log(hexHash);    
+  });
 };
 
-await calculateHash();
+calculateHash();
