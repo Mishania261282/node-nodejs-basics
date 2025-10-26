bff2cb002b811f766bb3fa53bbc7958185c5be56
Mikhail Kirpichou <mishania261282@.ru>
2025-10-26 18:27:06 +0300

    feat:add the solution for the file args.exe

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-  // Write your code here
+  const args = process.argv.slice(2);
+  const parsedArgs = [];  
+
+  for (let i = 0; i < args.length; i += 2) {
+    const keyArg = args[i];
+    const valueArg = args[i + 1];
+    const propName = keyArg.substring(2);
+    parsedArgs.push(`${propName} is ${valueArg}`);
+  }
+  const result = parsedArgs.join(", ");
+
+  console.log(result);
 };
 
 parseArgs();
