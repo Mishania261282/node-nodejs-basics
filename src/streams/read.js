fd48181af7cf1952b6d1011a53b6e4513e5b241f
Mikhail Kirpichou <mishania261282@.ru>
2025-10-26 21:20:03 +0300

    feat:add the solution for the file stream/read.exe

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
-const read = async () => {
-  // Write your code here
-};
+import { createReadStream } from "node:fs";
+import process from "node:process";
+import path from "node:path";
+import { fileURLToPath } from "url";
 
-await read();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const fileName = "fileToRead.txt";
+const filePath = path.join(__dirname, "files", fileName);
+const readStream = createReadStream(filePath, { encoding: "utf8" });
+
+readStream.on("data", (chunk) => {
+  process.stdout.write(chunk);
+});
+
+readStream.on("close", () => {
+  console.log("");
+  process.exit(0);
+});
